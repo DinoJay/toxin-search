17761dd3f9d8aa986dce636178ba575a028692ac
DinoJay <jott88@googlemail.com>
2022-01-20 18:03:15 +0100

    yeah

--- src/lib/filter/TestSpecies.svelte
+++ src/lib/filter/TestSpecies.svelte
@@ -11,15 +11,25 @@
 			<label class="flex items-center" for="targetOrgan">
 				<span class="mr-auto">Target Organ:</span>
 			</label>
-			<input class="border p-1" name="targetOrgan" id="targetOrgan" />
+			<input class="border pad" name="targetOrgan" id="targetOrgan" />
 		</div>
 		<div class="text-lg p-2 flex flex-col ">
 			<label class="flex items-center" for="sex"><span class="mr-auto">Sex:</span> </label>
-			<select class="border p-1" name="sex" id="type">
+			<select class="border py-2 px-1" name="sex" id="type">
 				<option value="all">All</option>
 				<option value="open">Female</option>
 				<option value="semi-occluded">Male </option>
 			</select>
 		</div>
+		<div class="text-lg p-2 mt-auto">
+			<button class="border pad px-2">Go</button>
+		</div>
 	</div>
 </Expandable>
+
+<style>
+	.pad {
+		padding-top: 4.9px;
+		padding-bottom: 4.9px;
+	}
+</style>
